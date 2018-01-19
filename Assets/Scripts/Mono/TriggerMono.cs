d4925c737bf689ff1c6d3e9a17bd411f71b912a5
brooshe <infinite.wxy@gmail.com>
2018-01-19 12:16:02 +0800

    Submit room system

---
+++ Assets/Scripts/Mono/TriggerMono.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMono : MonoBehaviour {
+
+	public delegate void TriggerDelegate (Collider other);
+	public TriggerDelegate OnTrigger;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (OnTrigger != null)
+			OnTrigger (other);
+	}
+}
